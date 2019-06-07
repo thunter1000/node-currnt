0829d8893aa894c8cd4d56e0a37e1239222e9aff
Thomas Hunter <th@thomashunter.co.uk>
2019-06-07 09:14:10 +0100

    Created example project

--- example/src/main.js
+++ example/src/main.js
@@ -1,2 +1,13 @@
-// ESM syntax is supported.
-export {};
+
+import Currnt from 'currnt';
+
+const dataToTransform = [...Array(30).keys()];
+
+new Currnt(async (d) => {
+  console.log(`Started\t\t🔥 ${d}`);
+  await new Promise(r => setTimeout(r, Math.floor(Math.random() * 2000) + 2000));
+  console.log(`Finished\t🛑 ${d}`);
+  return d * 2;
+}, dataToTransform).batch(5).run().then((result) => {
+  console.log('Result', result);
+});
